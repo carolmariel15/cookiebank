bacf4b7e30dee77f1efc4c7d6b59dec6522a76d5
Carol Mariel <carolmariel.14@gmail.com>
2022-06-19 16:29:39 -0700

    primer commit

---
+++ cookiebank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cookiebank
+//
+//  Created by Carol Mariel on 19/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
