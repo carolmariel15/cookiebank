109be85eea1a2cfb780f6e9ddf5dcccc9d69037e
Carol Mariel <carolmariel.14@gmail.com>
2022-06-19 18:58:02 -0700

    login con firebase

--- cookiebank/ViewController.swift
+++ cookiebank/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var txtClave: UITextField!
+    @IBOutlet weak var btnIngresar: UIButton!
+    @IBOutlet weak var btnRegistrar: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    
+    @IBAction func registrar(_ sender: Any) {
+        if let email = txtEmail.text, let clave = txtClave.text {
+            Auth.auth().createUser(withEmail: email, password: clave) {
+                (result, error) in
+                if let result = result, error == nil {
+                    let alertSuccess = UIAlertController(title: "Bienvenido(a)", message: "Se registro correctamente", preferredStyle: .alert)
+                    
+                    alertSuccess.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(alertSuccess, animated: true, completion: nil)
+                    self.txtEmail.text = ""
+                    self.txtClave.text = ""
+                    
+                } else {
+                    let alertError = UIAlertController(title: "Error", message: "Error al registrar cuenta (6 caracteres minimo)", preferredStyle: .alert)
+                    
+                    alertError.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(alertError, animated: true, completion: nil)
+                }
+            }
+        }
+        
+        
+    }
+    
 }
 
