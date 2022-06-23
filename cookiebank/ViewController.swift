0f861f6de07d2477bd609d380ee95864d09f5bb4
Carol Mariel <carolmariel.14@gmail.com>
2022-06-22 23:19:12 -0700

    login y pantalla registrar

--- cookiebank/ViewController.swift
+++ cookiebank/ViewController.swift
@@ -11,43 +11,38 @@ import FirebaseAuth
 class ViewController: UIViewController {
 
     @IBOutlet weak var txtEmail: UITextField!
-    
     @IBOutlet weak var txtClave: UITextField!
     @IBOutlet weak var btnIngresar: UIButton!
-    @IBOutlet weak var btnRegistrar: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     
     @IBAction func login(_ sender: Any) {
-    }
-    
-    @IBAction func registrar(_ sender: Any) {
         if let email = txtEmail.text, let clave = txtClave.text {
-            Auth.auth().createUser(withEmail: email, password: clave) {
+            Auth.auth().signIn(withEmail: email, password: clave) {
                 (result, error) in
                 if let result = result, error == nil {
-                    let alertSuccess = UIAlertController(title: "Bienvenido(a)", message: "Se registro correctamente", preferredStyle: .alert)
+                    let alertSuccess = UIAlertController(title: "Bienvenido(a)", message: "Inicio sesion correctamente", preferredStyle: .alert)
                     
                     alertSuccess.addAction(UIAlertAction(title: "Aceptar", style: .default))
                     self.present(alertSuccess, animated: true, completion: nil)
                     self.txtEmail.text = ""
                     self.txtClave.text = ""
                     
-                } else {
-                    let alertError = UIAlertController(title: "Error", message: "Error al registrar cuenta (contraseña 6 caracteres minimo)", preferredStyle: .alert)
+                }
+                else {
+                    let alertError = UIAlertController(title: "Error", message: "Error al iniciar sesion (clave 6 caracteres minimo)", preferredStyle: .alert)
                     
                     alertError.addAction(UIAlertAction(title: "Aceptar", style: .default))
                     self.present(alertError, animated: true, completion: nil)
                 }
             }
         }
-        
-        
     }
+    
+ 
     
 }
 
--- cookiebank/usuario/UsuarioViewController.swift
+++ cookiebank/usuario/UsuarioViewController.swift
@@ -6,24 +6,56 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class UsuarioViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtClaveInternet: UITextField!
+    @IBOutlet weak var btnRegistrar: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func registrarAuth () -> (String) {
+        var mensajeAlert = ""
+        
+        if let email = txtEmail.text, let clave = txtClaveInternet.text {
+            Auth.auth().createUser(withEmail: email, password: clave) {
+                (result, error) in
+                if let result = result, error == nil {
+                    mensajeAlert = "Se registro correctamente"
+                    
+                    let alertSuccess = UIAlertController(title: "Bienvenido(a)", message: "Se registro correctamente", preferredStyle: .alert)
+                    
+                    alertSuccess.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(alertSuccess, animated: true, completion: nil)
+                    self.txtEmail.text = ""
+                    self.txtClaveInternet.text = ""
+                    
+                } else {
+                    mensajeAlert = "Error al registrar cuenta (clave de internet 6 caracteres minimo)"
+                    
+                    let alertError = UIAlertController(title: "Error", message: "Error al registrar cuenta (contraseña 6 caracteres minimo)", preferredStyle: .alert)
+                    
+                    alertError.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    self.present(alertError, animated: true, completion: nil)
+                }
+            }
+        }
+        
+        return mensajeAlert
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registrar(_ sender: Any) {
+        
+        let x = registrarAuth()
+        
     }
-    */
+    
 
 }
